9333bd5f5619cbb31ed7fe42b272666a546aa24f
Yuriy Yurenko <yurenkoyura@gmail.com>
2023-03-10 06:54:30 +0200

    assets and layout

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('/', function () {
 
 
 Route::prefix('admin')->group(function () {
-    Route::get('dashboard',[DashboardController::class, 'index']);
+    Route::get('/',[DashboardController::class, 'index']);
 });
